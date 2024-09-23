17ca6df705bb11b5f45fd9e4eab7bab82101db29
scarlett <71671525+scarlett-mp4@users.noreply.github.com>
2024-09-23 09:27:42 -0400

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -193,21 +193,21 @@ step_start "Node.js"
   find /usr/local/include/node/openssl/archs -mindepth 1 -maxdepth 1 ! -name "$_opensslArch" -exec rm -rf {} \; >$__OUTPUT
   step_end "Node.js ${CLR_CYB}$NODE_VERSION${CLR} ${CLR_GN}Installed"
 
-step_start "Yarn"
-  export GNUPGHOME="$(mktemp -d)"
-  for key in 6A010C5166006599AA17F08146C2130DFD2497F5; do
-    gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ;
-  done
+# step_start "Yarn"
+#   export GNUPGHOME="$(mktemp -d)"
+#   for key in 6A010C5166006599AA17F08146C2130DFD2497F5; do
+#     gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ;
+#   done
 
-  os_fetch -O yarn-v$YARN_VERSION.tar.gz https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz
-  os_fetch -O yarn-v$YARN_VERSION.tar.gz.asc https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz.asc
-  gpg -q --batch --verify yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz >$__OUTPUT
-  gpgconf --kill all
-  tar -xzf yarn-v$YARN_VERSION.tar.gz -C /opt/
-  ln -sf /opt/yarn-v$YARN_VERSION/bin/yarn /usr/local/bin/yarn
-  ln -sf /opt/yarn-v$YARN_VERSION/bin/yarnpkg /usr/local/bin/yarnpkg
-  rm -rf "$GNUPGHOME" yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz
-  step_end "Yarn ${CLR_CYB}v$YARN_VERSION${CLR} ${CLR_GN}Installed"
+#   os_fetch -O yarn-v$YARN_VERSION.tar.gz https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz
+#   os_fetch -O yarn-v$YARN_VERSION.tar.gz.asc https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz.asc
+#   gpg -q --batch --verify yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz >$__OUTPUT
+#   gpgconf --kill all
+#   tar -xzf yarn-v$YARN_VERSION.tar.gz -C /opt/
+#   ln -sf /opt/yarn-v$YARN_VERSION/bin/yarn /usr/local/bin/yarn
+#   ln -sf /opt/yarn-v$YARN_VERSION/bin/yarnpkg /usr/local/bin/yarnpkg
+#   rm -rf "$GNUPGHOME" yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz
+#   step_end "Yarn ${CLR_CYB}v$YARN_VERSION${CLR} ${CLR_GN}Installed"
 
 step_start "Nginx Proxy Manager" "Downloading" "Downloaded"
   NPM_VERSION=$(os_fetch -O- https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
